2253b43d379e180d940befac5bd7655cfbd59a4f
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-03-31 00:47:32 +0700

    test husky

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { Button } from '@/components/ui/button'
+import { movieService } from './services/movieService';
 function App() {
   return (
+    console.log(movieService);
     <div className='text-center mx-auto '>
       <h1 className='text-3xl font-bold text-primary-50'>This is our OOSE project: HDmoVie</h1>
       <Button className='my-10'>Demo Shadcn</Button>
