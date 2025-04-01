817ca5ee3c23c89c8d4a7cfbce618334c813b5e5
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-04-01 12:28:45 +0700

    test husky

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { Button } from '@/components/ui/button'
 function App() {
-  console.log('object')
   return (
     <div className='text-center mx-auto '>
       <h1 className='text-3xl font-bold rounded-big'>This is our OOSE project: HDmoVie</h1>
