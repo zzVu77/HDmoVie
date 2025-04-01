3c522b5effb67543ebcb4b1d2f42e900f95368f6
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-04-01 12:36:26 +0700

    test husky

--- frontend/lint-staged.config.mjs
+++ frontend/lint-staged.config.mjs
@@ -4,8 +4,8 @@
  */
 
 const lintStagedConfig = {
-  "./**/*.{js,jsx,mjs,cjs,ts,tsx}": [() => "npm run lint", () => "tsc --noEmit", "eslint --fix", "prettier --write"],
-  "*.{css,scss,json,md,mdx,html}": ["prettier --write"],
-};
+  './**/*.{js,jsx,mjs,cjs,ts,tsx}': [() => 'npm run lint', () => 'tsc --noEmit', 'eslint --fix', 'prettier --write'],
+  '*.{css,scss,json,md,mdx,html}': ['prettier --write'],
+}
 
-export default lintStagedConfig;
+export default lintStagedConfig
