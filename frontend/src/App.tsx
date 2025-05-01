89c41991b88832b68c821d2c5ec529c53949d890
kiyotaka1102 <phantrieuhuy2004@gmail.com>
2025-05-01 18:19:44 +0700

    feat: add blog component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import NotFound404 from './pages/NotFound404'
 import TestComponent from './pages/TestComponent'
+import BlogPage from './pages/Blog'
+
 function App() {
   return (
     <BrowserRouter>
@@ -13,6 +15,7 @@ function App() {
         <Route path='*' element={<NotFound404 />} />
         <Route path='/login' element={<Login />} />
         <Route path='/test' element={<TestComponent />} />
+        <Route path='/blog' element={<BlogPage />} />
       </Routes>
     </BrowserRouter>
   )
