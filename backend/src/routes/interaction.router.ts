172205508e9726dab5c036ad81f647433eb282ee
kiyotaka1102 <phantrieuhuy2004@gmail.com>
2025-05-03 09:34:59 +0700

    clean code

--- backend/src/routes/interaction.router.ts
+++ backend/src/routes/interaction.router.ts
@@ -1,18 +1,11 @@
 import { Router } from 'express'
 import { InteractionController } from '~/controllers/interaction.controller'
 import { AppDataSource } from '~/data-source'
-import { RegisteredUserRepository } from '~/repositories/registeredUser.repository'
-import { BlogRepository } from '~/repositories/blog.repository'
-import { LikeInteractionRepository } from '~/repositories/likeInteraction.repository'
 import { InteractionService } from '~/services/interaction.service'
 import { likeOrUnlikeMiddleware } from '~/middlewares/likeInteraction.middleware'
 
 const interactionRouter = Router()
 
-// Khởi tạo các dependencies
-const userRepository = new RegisteredUserRepository(AppDataSource)
-const blogRepository = new BlogRepository(AppDataSource)
-const likeInteractionRepository = new LikeInteractionRepository(AppDataSource)
 const interactionService = new InteractionService(AppDataSource)
 const interactionController = new InteractionController(interactionService)
 
