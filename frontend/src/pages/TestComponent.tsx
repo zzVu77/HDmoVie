bd7b7d278ce70e43cb11bd8e743d07b55ab08729
Bien Xuan Huy <bienxhuy@gmail.com>
2025-05-10 16:56:09 +0700

    fix: Editing blog card media display

--- frontend/src/pages/TestComponent.tsx
+++ frontend/src/pages/TestComponent.tsx
@@ -256,6 +256,8 @@ export const sampleBlogs = [
     images: [
       'https://fastly.picsum.photos/id/866/536/354.jpg?hmac=tGofDTV7tl2rprappPzKFiZ9vDh5MKj39oa2D--gqhA',
       'https://fastly.picsum.photos/id/866/536/354.jpg?hmac=tGofDTV7tl2rprappPzKFiZ9vDh5MKj39oa2D--gqhA',
+      'https://makerworld.bblmw.com/makerworld/model/US2ab61bb7d3000c/design/2024-01-30_029b2304056c.png?x-oss-process=image/resize,w_1000/format,webp',
+      'https://fastly.picsum.photos/id/866/536/354.jpg?hmac=tGofDTV7tl2rprappPzKFiZ9vDh5MKj39oa2D--gqhA',
     ],
   },
   {
