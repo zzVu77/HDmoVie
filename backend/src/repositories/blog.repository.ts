c2c0f67966ac2b1258c20ff716303b374a243436
Bien Xuan Huy <bienxhuy@gmail.com>
2025-05-13 18:42:58 +0700

    fix: Adjust find blog by user id in blog repo to get like count and comment count

--- backend/src/repositories/blog.repository.ts
+++ backend/src/repositories/blog.repository.ts
@@ -77,16 +77,41 @@ export class BlogRepository {
   }
 
   async findByUserId(userId: string, offset: number, limit: number): Promise<Blog[]> {
-    try {
-      return this.repository.find({
-        where: { owner: { id: userId } } as FindOptionsWhere<Blog>,
-        skip: offset,
-        take: limit,
-        relations: ['tags', 'owner', 'imageUrls'],
-        order: { dateCreated: 'DESC' },
-      })
-    } catch (error) {
-      throw new Error(`Failed to find blogs by user ID: ${(error as Error).message}`)
-    }
+    const blogs = await this.repository
+      .createQueryBuilder('blog')
+      .select([
+        'blog.id',
+        'blog.content',
+        'blog.dateCreated',
+        'owner.id',
+        'owner.fullName',
+        'tags.id',
+        'tags.name',
+        'imageUrls.id',
+        'imageUrls.url',
+      ])
+      .addSelect(
+        `(SELECT COUNT(*) 
+        FROM like_interactions_users liu 
+        JOIN like_interactions li ON liu.likeInteractionId = li.id 
+        WHERE li.blogId = blog.id)`,
+        'likeCount',
+      )
+      .addSelect(
+        `(SELECT COUNT(*) 
+        FROM comments c 
+        WHERE c.blogId = blog.id AND c.type = 'BLOG')`,
+        'commentCount',
+      )
+      .leftJoin('blog.owner', 'owner')
+      .leftJoin('blog.tags', 'tags')
+      .leftJoin('blog.imageUrls', 'imageUrls')
+      .where('owner.id = :userId', { userId })
+      .orderBy('blog.dateCreated', 'DESC')
+      .skip(offset)
+      .take(limit)
+      .getMany()
+
+    return blogs
   }
 }
