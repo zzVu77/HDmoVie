35023bf2dc5108dce54669be8af84a8276265d3f
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-05-21 16:04:36 +0700

    fix: Update CORS configuration to allow requests from Netlify

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,7 +24,7 @@ dotenv.config()
 const app = express()
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://hdmovie-oose.netlify.app'],
     credentials: true,
   }),
 )
