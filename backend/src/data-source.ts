e6ebd9e4cbb157d5886bf48938292ed0770dd4b1
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-05-25 09:30:46 +0700

    refactor: Implement singleton pattern for DataSource initialization

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,17 +1,31 @@
 import { DataSource } from 'typeorm'
 import path from 'path'
 
-export const AppDataSource = new DataSource({
-  type: 'mysql',
-  host: 'localhost', // hoặc 'mysql' nếu dùng Docker
-  port: 33061,
-  username: 'root',
-  password: '123456',
-  database: 'HDmoVie',
-  synchronize: false,
-  logging: true,
-  entities: [path.join(__dirname, 'models/**/*.{js,ts}')],
-  migrations: [path.join(__dirname, 'migrations/**/*.{js,ts}')],
-  migrationsTableName: 'migrations',
-  subscribers: [path.join(__dirname, 'subscribers/**/*.{js,ts}')],
-})
+class Database {
+  private static instance: DataSource
+
+  private constructor() {}
+
+  public static getInstance(): DataSource {
+    if (!Database.instance) {
+      Database.instance = new DataSource({
+        type: 'mysql',
+        host: 'localhost', // hoặc 'mysql' nếu dùng Docker
+        port: 33061,
+        username: 'root',
+        password: '123456',
+        database: 'HDmoVie',
+        synchronize: false,
+        logging: true,
+        entities: [path.join(__dirname, 'models/**/*.{js,ts}')],
+        migrations: [path.join(__dirname, 'migrations/**/*.{js,ts}')],
+        migrationsTableName: 'migrations',
+        subscribers: [path.join(__dirname, 'subscribers/**/*.{js,ts}')],
+      })
+    }
+    return Database.instance
+  }
+}
+
+export default Database
+export const AppDataSource = Database.getInstance()
