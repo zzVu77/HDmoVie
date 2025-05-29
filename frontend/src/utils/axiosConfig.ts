4be67685cf6394ec0c004c9e8b382c6d7d3ddc7b
zzVu77 <nguyenvanvu24062004@gmail.com>
2025-05-29 21:52:38 +0700

    fix: conditionally render 'Add to Watchlist' button based on exploreNow prop

--- frontend/src/utils/axiosConfig.ts
+++ frontend/src/utils/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, InternalAxiosRequestConfig } from 'axios'
 
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json',
   },
